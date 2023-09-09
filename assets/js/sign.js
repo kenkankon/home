7e4826ea31bb097b062e3ea64943fd3ebf6a7872
kenkankon <144560618+kenkankon@users.noreply.github.com>
2023-09-09 23:00:24 +0300

    Add files via upload

---
+++ assets/js/sign.js
@@ -0,0 +1,21 @@
+let sup = document.querySelector('#hiddin');
+let sin = document.querySelector('#one');
+
+let supbtn = document.getElementById('sig');
+let sinbtn = document.getElementById('log');
+
+
+supbtn.addEventListener('click', function () {
+
+  sin.style.display = 'none';
+  sup.style.display = 'block';
+ 
+});
+
+
+
+sinbtn.addEventListener('click', function () {
+
+  sin.style.display = 'none';
+  sup.style.display = 'block';
+});
